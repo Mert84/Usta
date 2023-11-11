06763b5befe6a0e150b7317252f9e5793f342254
Mert Büyüktuncay <mert.buyuktuncay@sestek.com>
2023-11-11 15:18:56 +0300

    added secure tls options & ca roots certificates

--- src/SampleHttpClient/Tester.h
+++ src/SampleHttpClient/Tester.h
@@ -59,11 +59,25 @@ struct Tester
 	{
 		using namespace boost::asio::ssl;
 		auto sslContext = std::make_shared<context>(context::tlsv12);
-		sslContext->set_verify_mode(verify_none);
+
+		sslContext->load_verify_file("cacert.pem");
+		sslContext->set_verify_mode(verify_peer);
 
 		sslContext->set_verify_callback([](bool preverified, verify_context & verifyContext) {			
-			return true;
+			return preverified;
 		});
+
+		const char* defaultCipherList = "HIGH:!ADH:!MD5:!RC4:!SRP:!PSK:!DSS";
+		auto ret = SSL_CTX_set_cipher_list(sslContext->native_handle(), defaultCipherList);
+
+		auto options =
+			boost::asio::ssl::context::default_workarounds |
+			boost::asio::ssl::context::no_sslv2 |
+			boost::asio::ssl::context::no_sslv3 |
+			boost::asio::ssl::context::no_tlsv1 |
+			boost::asio::ssl::context::no_tlsv1_1;
+		sslContext->set_options(options);
+
 
 		return sslContext;
 	}
