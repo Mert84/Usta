39c382b2e12205d4f6ce950a775d09b4afe8d4ef
Mert Büyüktuncay <mert.buyuktuncay@sestek.com>
2024-01-27 14:04:54 +0300

    implementting stress test

--- src/HttpClientStressTest/HttpClientStressTest.cpp
+++ src/HttpClientStressTest/HttpClientStressTest.cpp
@@ -4,15 +4,45 @@
 #include <boost/asio.hpp>
 #include "StressTester.h"
 
-int main()
+
+std::shared_ptr<boost::asio::ssl::context> PrepareSslContext()
+{
+    using namespace boost::asio::ssl;
+    auto sslContext = std::make_shared<context>(context::tlsv12);
+
+    sslContext->load_verify_file("cacert.pem");
+    sslContext->set_verify_mode(verify_peer);
+
+    sslContext->set_verify_callback([](bool preverified, verify_context& verifyContext) {
+        return preverified;
+        });
+
+    const char* defaultCipherList = "HIGH:!ADH:!MD5:!RC4:!SRP:!PSK:!DSS";
+    auto ret = SSL_CTX_set_cipher_list(sslContext->native_handle(), defaultCipherList);
+
+    auto options =
+        boost::asio::ssl::context::default_workarounds |
+        boost::asio::ssl::context::no_sslv2 |
+        boost::asio::ssl::context::no_sslv3 |
+        boost::asio::ssl::context::no_tlsv1 |
+        boost::asio::ssl::context::no_tlsv1_1;
+    sslContext->set_options(options);
+
+
+    return sslContext;
+}
+
+void test()
 {
     boost::asio::io_context ioContext;
     std::cout << "started" << std::endl;
 
     {
+        auto sslContext = PrepareSslContext();
+
         auto dummyWork = std::make_unique<boost::asio::io_context::work>(ioContext);
 
-        StressTester stressTester(ioContext);
+        StressTester stressTester(ioContext, sslContext);
         stressTester();
 
         std::thread th([&ioContext]() {
@@ -40,4 +70,19 @@ int main()
     }
 
     std::cout << "done." << std::endl;
+}
+
+int main()
+{
+    try
+    {
+        test();
+    }
+    catch (const std::exception & ex)
+    {
+        std::cout << "exception occurred during test. Exception message : " << ex.what() << std::endl;
+
+    }
+
+    return 0;
 }
--- src/HttpClientStressTest/StressTester.h
+++ src/HttpClientStressTest/StressTester.h
@@ -6,16 +6,72 @@
 #include "../UstaCommon/HttpRequest.h"
 #include "../UstaCommon/HttpResponse.h"
 
+inline bool Validate(HttpResponse& response)
+{
+	if (response.StatusCode() == 200)
+	{
+		return true;
+	}
+
+	return false;
+}
+
 struct StressTester
 {
 	boost::asio::any_io_executor _executor;
+	std::shared_ptr<boost::asio::ssl::context> _sslContext;
 
-	explicit StressTester(boost::asio::io_context & ioContext)
+	explicit StressTester(boost::asio::io_context & ioContext,
+		std::shared_ptr<boost::asio::ssl::context> sslContext)
 		:
-		_executor(ioContext.get_executor())
+		_executor(ioContext.get_executor()),
+		_sslContext(move(sslContext))
 	{}
 
 	void operator()()
 	{
+		HttpClientParameters parameters;
+		parameters._executor = _executor;
+		parameters._sslContext = _sslContext;
+
+		auto httpClient = HttpClient::Make(std::move(parameters));
+
+		std::string link = "http://localhost/a.txt";
+
+		ConnectionParameter connectionParameterBetter =
+			make_connection_parameter(link);
+
+		httpClient->ConnectAsync(std::move(connectionParameterBetter),
+			[httpClient, link](std::error_code err) {
+				if (err)
+				{
+					std::cout << "ConnectAsync failed" << err.message() << std::endl;
+					return;
+				}
+
+				std::cout << "ConnectAsync succeeded" << std::endl;
+
+				HttpRequest request = make_get_request(link);
+
+				httpClient->SendAsync(request,
+					[](std::error_code err, HttpResponse response) {
+						if (err)
+						{
+							std::cout << "error occurred. Error message: " << err.message() << std::endl;
+							return;
+						}
+
+
+						auto valid = Validate(response);
+						if (valid)
+						{
+							std::cout << "Request succeeded. " << std::endl;
+						}
+						else
+						{
+							std::cout << "Request failed. " << std::endl;
+						}
+					});
+			});
 	}
 };
